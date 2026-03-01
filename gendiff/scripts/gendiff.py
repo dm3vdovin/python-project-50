237bcb1c20b206ede4052d0f012c0922d91c52e8
Dmitry Vdovin <dm3vdovin@gmail.com>
2026-03-01 21:05:02 +0300

    Add files

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -1,10 +1,14 @@
 from argparse import ArgumentParser
+import json
 
+
+def parse_json(file_path):
+    return json.load(file_path)
 
 def main():
     parser = ArgumentParser(
-        prog='hexlet-code',
-        description='Compares two configuration files and shows a difference.'
+        prog = 'gendiff',
+        description = 'Compares two configuration files and shows a difference.'
     )
     parser.add_argument('first_file')
     parser.add_argument('second_file')
@@ -14,7 +18,10 @@ def main():
         help = 'set format of output'
     )
 
-    parser.parse_args()
+    args = parser.parse_args()
+
+    print(parse_json(args.first_file))
+    print(parse_json(args.second_file))
 
 
 if __name__ == '__main__':
